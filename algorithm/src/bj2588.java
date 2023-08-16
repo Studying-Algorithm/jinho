185b7ec1b84ae1f9cc673d4dc19099c94b9516e3
wlxorjs2 <49912284+wlxorjs2@users.noreply.github.com>
2023-08-16 21:56:26 +0900

    Update bj2588.java

--- algorithm/src/bj2588.java
+++ algorithm/src/bj2588.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class bj2588 {
+import java.util.Scanner;
+public class bj2588 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int sum = 0;
+        int dap = a * b;
+
+        for(int i = 0; i < 3; i++) {
+            sum = (b % 10) * a;
+            System.out.println(sum);
+            b /= 10;
+        }
+        System.out.println(dap);
+    }
 }
